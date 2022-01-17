4c6d969fb548a5f16a4a3a762608717cedfa8373
Camila Camacho <mccamacho79@misena.edu.co>
2022-01-16 22:13:30 -0500

    ajustes

--- APIOracle/APIOracle.DataAccess/Models/ModelContext.cs
+++ APIOracle/APIOracle.DataAccess/Models/ModelContext.cs
@@ -21,11 +21,6 @@ namespace APIOracle.DataAccess.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseOracle("user id=pruebaTecnica;password=Camila1030;data source=localhost:1521/ORCL;");
-//            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
