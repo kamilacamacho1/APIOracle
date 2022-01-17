5751a10117f4febd95e0ac2525c342fdf71a5aa0
Camila Camacho <mccamacho79@misena.edu.co>
2022-01-16 19:46:28 -0500

    Validaciones

--- APIOracle/APIOracle.WebApplication/Models/Autor/AutorEntity.cs
+++ APIOracle/APIOracle.WebApplication/Models/Autor/AutorEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,12 @@ namespace APIOracle.WebApplication.Models.Autor
     public class AutorEntity
     {
         public int Idautor { get; set; }
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
         public string Nombre { get; set; }
         public DateTime Fechanacimiento { get; set; }
+        [Required(ErrorMessage = "La Ciudad es obligatorio")]
         public string Ciudad { get; set; }
+        [Required(ErrorMessage = "El corre es obligatorio")]
         public string Correo { get; set; }
     }
 }
--- APIOracle/APIOracle.WebApplication/Models/Libro/LibroEntity.cs
+++ APIOracle/APIOracle.WebApplication/Models/Libro/LibroEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace APIOracle.WebApplication.Models.Libro
     public class LibroEntity
     {
         public int Idlibro { get; set; }
+        [Required(ErrorMessage = "El corre es obligatorio")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "El corre es obligatorio")]
         public string Anio { get; set; }
+        [Required(ErrorMessage = "El corre es obligatorio")]
         public string Genero { get; set; }
         public int Nopaginas { get; set; }
         public int Idautor { get; set; }
